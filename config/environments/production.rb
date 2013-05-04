025f9635d7a497de5f7e6da856dcfea4769c5d5f
James Yu <supersecretjames@gmail.com>
2013-05-04 08:40:49 -0700

    remove s3 host name from production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,7 @@ DishRank::Application.configure do
   :s3_credentials => {
     :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-    :s3_host_name => ":s3-website-us-west-2.amazonaws.com"
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
 end
