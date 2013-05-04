377c70d62035125bdbdb87566cfeb90a2152c913
James Yu <supersecretjames@gmail.com>
2013-05-04 09:11:31 -0700

    moved paperclip url defaults

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,11 +67,11 @@ DishRank::Application.configure do
 
   config.paperclip_defaults = {
   :storage => :s3,
+  :url => ":s3_domain_url",
   :s3_credentials => {
     :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-    :url => ":s3_domain_url"
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
 end
