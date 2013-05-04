a7ceb80b47245914dd8f71a7e3568714a64b0da3
James Yu <supersecretjames@gmail.com>
2013-05-04 08:30:09 -0700

    test aws fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,8 @@ DishRank::Application.configure do
   :s3_credentials => {
     :bucket => ENV['AWS_BUCKET'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+    :s3_host_name: "s3-website-us-west-2.amazonaws.com"
   }
 }
 end
